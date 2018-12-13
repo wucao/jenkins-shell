696e1039b38d1d0c547ba73f14fbf23a20cc2a57
吴操 <wucao@qiyi.com>
2018-12-13 16:00:52 +0800

    init

---
+++ java_project.sh
@@ -0,0 +1,37 @@
+PROJECT_UUID=${JOB_NAME##*/}
+SERVER_IPS_ARR=(${SERVER_IPS//,/ })
+
+if [ "$EVENT" = "kill" ]; then
+
+  for IP in ${SERVER_IPS_ARR[@]}
+  do
+      ssh $IP "pkill -f $PROJECT_UUID" || true
+  done
+
+else
+
+  if [ "$MVN_PROFILE" = "" ]; then
+      /opt/maven/bin/mvn clean package -Dmaven.test.skip=true
+  else
+      /opt/maven/bin/mvn clean package -Dmaven.test.skip=true -P$MVN_PROFILE
+  fi
+
+
+  JAR_FILE=$(find target -type f -name "*.jar" -maxdepth 1 -exec du '{}' \; | sort -nrk 1 | head -n 1 | cut -f 2)
+
+
+  for IP in ${SERVER_IPS_ARR[@]}
+  do
+      ssh $IP "pkill -f $PROJECT_UUID" || true
+      ssh $IP "
+          rm -rf /data/webserver/$PROJECT_UUID
+          mkdir -p /data/webserver/$PROJECT_UUID
+      "
+      scp $JAR_FILE $IP:/data/webserver/$PROJECT_UUID/root.jar
+      ssh $IP "
+          cd /data/webserver/$PROJECT_UUID
+          nohup java -XX:-OmitStackTraceInFastThrow -jar /data/webserver/$PROJECT_UUID/root.jar > nohup.out 2>&1 &
+      "
+  done
+
+fi
---
+++ jetty_project.sh
@@ -0,0 +1,38 @@
+PROJECT_UUID=${JOB_NAME##*/}
+SERVER_IPS_ARR=(${SERVER_IPS//,/ })
+
+if [ "$EVENT" = "kill" ]; then
+
+  for IP in ${SERVER_IPS_ARR[@]}
+  do
+      ssh $IP "pkill -f $PROJECT_UUID" || true
+  done
+
+else
+
+  if [ "$MVN_PROFILE" = "" ]; then
+      /opt/maven/bin/mvn clean package -Dmaven.test.skip=true
+  else
+      /opt/maven/bin/mvn clean package -Dmaven.test.skip=true -P$MVN_PROFILE
+  fi
+
+  WAR_FILE=$(find target -type f -name "*.war" -maxdepth 1)
+
+  for IP in ${SERVER_IPS_ARR[@]}
+  do
+      ssh $IP "pkill -f $PROJECT_UUID" || true
+      ssh $IP "
+          rm -rf /data/webserver/$PROJECT_UUID
+          mkdir -p /data/webserver/$PROJECT_UUID
+          cd /data/webserver/$PROJECT_UUID
+          java -jar /opt/jetty/start.jar --add-to-startd=http,deploy,jsp,websocket,http-forwarded
+      "
+      scp $WAR_FILE $IP:/data/webserver/$PROJECT_UUID/webapps/$CONTENT_PATH.war
+      ssh $IP "
+          cd /data/webserver/$PROJECT_UUID
+          mkdir -p /data/webserver/temp
+          nohup java -XX:-OmitStackTraceInFastThrow -Djava.io.tmpdir=/data/webserver/temp -jar /opt/jetty/start.jar jetty.port=$JETTY_PORT projectuuid=$PROJECT_UUID > nohup.out 2>&1 &
+      "
+  done
+
+fi
